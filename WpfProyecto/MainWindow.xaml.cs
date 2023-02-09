3fa47c6ded024820649fd0bf345dab85e5c9e385
Yous <guryxxx@hotmail.com>
2023-02-09 23:21:39 +0100

    Revert "Añado TODAS las palabras de 5 letras."
    This reverts commit b2eb36a090fe17cfbf7439c270d10b6d4a46ea93.

--- WpfProyecto/MainWindow.xaml.cs
+++ WpfProyecto/MainWindow.xaml.cs
@@ -58,8 +58,8 @@ namespace WpfProyecto
             }
 
             Random generador = new Random();
-            int numeroAleatorio = generador.Next(1, wordList.Count() - 1);
-            SecretWord = wordList[numeroAleatorio];
+            int numeroAleatorio = generador.Next(1, 6);
+            SecretWord = "PIZZA";
             char[] SecretWordArray = SecretWord.ToCharArray();
 
             debug.Content = SecretWord;
